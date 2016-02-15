61331027b6ad2d8177e18f7308d56703700098e6
Michael Wood <mww012@gmail.com>
2016-02-15 13:51:18 -0600

    Initial commit

---
+++ aruba_apfinder.py
@@ -0,0 +1,65 @@
+"""
+This script is intended to be used in an Aruba Networks wireless environment.
+"""
+
+import argparse
+import netaddr
+import sys
+import threading
+
+
+def ipv4(string):
+    return str(netaddr.IPAddress(string))
+
+
+def mac_addr(string):
+    return str(netaddr.EUI(string))
+
+def main():
+    parser = argparse.ArgumentParser(description=(
+        'This script will do at least one'
+        ' of the following for a given sta:\n'
+        '- Notify user of the current associated AP via the Pushover '
+        'notification service\n'
+        '- Blink the lights on the currently associated AP\n'
+        '- Write the AP associations a times to a log file'),
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-m', '--master', required=True, metavar="IP",
+                        type=ipv4, help="Master Controller IP Address")
+    parser.add_argument('-e', '--enable', required=True, metavar="ENABLEPW",
+                        help='Controller enable password')
+    parser.add_argument('-t', '--targetsta', required=True, metavar="STAMAC",
+                        type=mac_addr,
+                        help="Station mac address you want to target")
+    parser.add_argument('-u', '--user', required=True, type=str,
+                        help="SSH Username")
+    parser.add_argument('-pw', '--password', required=True,
+                        help="SSH Password")
+    parser.add_argument('-b', '--blink',action='store_true',
+                        help="Blink currently associated AP")
+    parser.add_argument('-l', '--log', type=argparse.FileType('w'),
+                        help='Logs station associations and timestamps to a '
+                             'given file')
+    pushover = parser.add_argument_group('Pushover Notifications',
+                                         'Configuration to send push '
+                                         'notifications to the Pushover '
+                                         'service')
+    pushover.add_argument('-p', '--pushover',
+                          help="Utilize Pushover service for current AP "
+                               "notification")
+    pushover.add_argument('-pt', '--pushtoken', metavar="TOKEN",
+                          help="Pushover Application Token")
+    pushover.add_argument('-pk', '--pushkey', metavar="KEY",
+                          help="Pushover User Key")
+
+    arguments = parser.parse_args()
+    print arguments
+    import aruba_ssh
+    controllers = aruba_ssh.enum_controllers(arguments.master, arguments.user,
+                               arguments.password, arguments.enable)
+    for controller in controllers:
+        print controller
+
+if __name__=="__main__":
+    sys.exit(main())
---
+++ aruba_ssh.py
@@ -0,0 +1,36 @@
+import paramiko
+import time
+import re
+
+
+def enum_controllers(master, user, pw, enpw):
+    output = ''
+    conn = connect_ssh(master, user, pw)
+    print "enum_controllers"
+    sh = conn.invoke_shell()
+    time.sleep(0.5)
+
+    # enable prompt
+    sh.send('en\n')
+    time.sleep(0.5)
+    sh.send(enpw)
+    sh.send('\n')
+    time.sleep(0.5)
+
+    # get local switches
+    while sh.recv_ready():
+        sh.recv(1024)
+    sh.send('show switches\n')
+    time.sleep(0.5)
+    output += sh.recv(1024)
+    controllers = re.findall(r"(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)", output)
+    return controllers
+
+
+def connect_ssh(ip, user, pw):
+    print "connect_ssh"
+    conn = paramiko.SSHClient()
+    conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    conn.connect(ip, 22, user, pw)
+    return conn
+
